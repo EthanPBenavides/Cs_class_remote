507fcc14855bdf51f1ca93fcad20211ca0462d96
EthanPBenavides <EthanPBenavides@gmail.com>
2021-12-01 12:54:26 -0600

    worked on guessing game

--- unit 1/While loops/GuessingGame.java
+++ unit 1/While loops/GuessingGame.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 public class GuessingGame
 {
@@ -6,17 +6,37 @@ public class GuessingGame
 
 	public GuessingGame(int stop)
 	{
+		upperBound = stop;
+	}
+
+	public void setUpper(int upper)
+	{
+		upperBound = upper;
+	}
+
+	public int getUpper()
+	{
+		return upperBound;
 	}
 
 	public void playGame()
 	{
 		Scanner keyboard = new Scanner(System.in);
 		/*Yes, you can have System.out.println in this method */
-	}
+		int guess  = 0;
+		int count  = 0;
+		int secret = (int) (Math.random()*(upperBound + 1));
+		do 
+		{
+			System.out.println("What is the number? ");
+			guess = keyboard.nextInt();
+			count++;
+			keyboard.next();
+		} 
+		while ( guess != secret);
+		keyboard.close();
+		System.out.println("It took "+count+"guesses to guess "+secret+".");
+		
 
-	public String toString()
-	{
-		String output= "" + upperBound;
-		return output;
 	}
 }
