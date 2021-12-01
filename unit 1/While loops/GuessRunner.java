f73e32f362c1504cdcdd780f7ec3ce4b3949eb5e
EthanPBenavides <EthanPBenavides@gmail.com>
2021-12-01 12:54:43 -0600

    worked on guessing game

--- unit 1/While loops/GuessRunner.java
+++ unit 1/While loops/GuessRunner.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /* It should go without saying that you need keyboard input to do this lab */
 
@@ -5,7 +6,16 @@ public class GuessRunner
 {
 	public static void main(String args[])
 	{
+		boolean cont = true;
+		int nums = 0;
 		Scanner keyboard = new Scanner(System.in);
-		char response;
+		GuessingGame g=new GuessingGame(nums);
+		do 
+		{
+			System.out.println("Guessing game - how many numbers? ");
+			nums = keyboard.nextInt();
+			g.playGame();
+		}
+		while (cont);
 	}
 }
